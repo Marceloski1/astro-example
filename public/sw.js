6bc837e3ddc6cb5f700aa6de0e413cf5932c3358
Eduardo Marcelo Mazzola Fernádez <eduardo.mmf@avangenio.com>
2026-02-19 22:42:00 -0500

    initial commit: astro learning project with 3d components and custom pages

---
+++ public/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'astro-books-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/favicon.svg',
+  '/styles/global.css',
+  '/personal',
+  '/favoritos',
+  '/design-system'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          // Cachear dinámicamente nuevas páginas visitadas
+          if (event.request.method === 'GET' && fetchResponse.status === 200) {
+            cache.put(event.request, fetchResponse.clone());
+          }
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      // Offline fallback simple si no hay cache ni red
+      return caches.match('/');
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
